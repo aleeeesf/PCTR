06b9598f7d36e3ad8f71573791ca950c91ae7705
Alejandro Serrano <alejandroserfer@gmail.com>
2020-10-27 13:23:54 +0100

    Practica 5

--- Practica 5/src/numPerfectos.java
+++ Practica 5/src/numPerfectos.java
@@ -21,81 +21,63 @@ public class numPerfectos implements Callable<Integer>{
 	{
 		int aux = 0, cont = 0;
 		
-		for(int i = inicio; i < fin; i++)				
+		for(int i = inicio; i <= fin; i++)				
 		{
 			if(i % 2 == 0)
 			{
-				for(int j = (i/2); j >= 1; j++)
+				aux = 0;
+				for(int j = 1; j <= (i/2) ; j++)
 				{
 					if(i % j == 0)
 					{
 						aux = aux+j;
 					}
-				}	
+				}
 				
-				if(aux == 1) cont++;
+				if(aux == i && aux != 0) 
+				{
+					System.out.println(i);
+					cont++;
+				}
 			}
 		}
-		
-		return cont;
-		
+
+		return cont;		
 	}
 	
-	public Integer perfectos(int fin)
-	{
-		int aux = 0, cont = 0;
-		
-		for(int i = 0; i <= fin; i++)				
-		{
-			if(i % 2 == 0)
-			{
-				for(int j = (i/2); j >= 1; j++)
-				{
-					if(i % j == 0)
-					{
-						aux = aux+j;
-					}
-				}	
-				
-				if(aux == 1) cont++;
-			}
-		}
-		
-		return cont;
-		
-	}
 	public static void main(String args[]) throws InterruptedException, ExecutionException
 	{
 		Runtime r = Runtime.getRuntime();
 		int nThreads = r.availableProcessors();
-		int rango = 30;			
+		int rango = Integer.parseInt(args[0]);			
 		
 		ThreadPoolExecutor ex = new ThreadPoolExecutor(nThreads,nThreads,0L,TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
 		ArrayList<Future<Integer>>  v= new ArrayList<Future<Integer>>();
-		int ini = 0, fin = rango/nThreads;
+		int ini = 1, fin = rango/nThreads;		
 		
 		
 		for(int i = 0; i < nThreads; i++)
 		{
+			System.out.println("inicio:"+ini);
+			System.out.println("fin:"+fin);
 			numPerfectos n = new numPerfectos(ini, fin);
 			v.add(ex.submit(n));
-			ini = fin;
-			fin += rango/nThreads;
-
+			ini = fin + 1;
+			if(i == nThreads-2)
+				fin = rango;
+			else
+				fin += rango/nThreads;
 		}			
 
 		int nPerfectos = 0;
 		for(Future<Integer> i: v)
 		{
-			System.out.println("mm");
 			nPerfectos += i.get();
 		}
 		
 		ex.shutdown();
 		
 		while(!ex.isTerminated());
-		System.out.println("Hay"+nPerfectos+"Numeros perfectos");
-		numPerfectos n = new numPerfectos(ini, fin);
-		System.out.println(n.perfectos(7));
+		System.out.println("Hay"+nPerfectos+"Numeros perfectos");		
 	}
 }
